ccb030a848453dab866ab017e5189ba7d11430a4
bkk711 <boylaboy8@gmail.com>
2019-06-22 12:06:08 +0700

    Add files via upload

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_song = new System.Windows.Forms.Label();
+            this.lb_singer = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.pic_cs = new System.Windows.Forms.PictureBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(54, 49);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(340, 22);
+            this.txt_search.TabIndex = 0;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(433, 47);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.Btn_search_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name of Song: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Singer : ";
+            // 
+            // lb_song
+            // 
+            this.lb_song.AutoSize = true;
+            this.lb_song.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_song.Location = new System.Drawing.Point(236, 92);
+            this.lb_song.Name = "lb_song";
+            this.lb_song.Size = new System.Drawing.Size(57, 25);
+            this.lb_song.TabIndex = 6;
+            this.lb_song.Text = ".........";
+            // 
+            // lb_singer
+            // 
+            this.lb_singer.AutoSize = true;
+            this.lb_singer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_singer.Location = new System.Drawing.Point(236, 142);
+            this.lb_singer.Name = "lb_singer";
+            this.lb_singer.Size = new System.Drawing.Size(57, 25);
+            this.lb_singer.TabIndex = 7;
+            this.lb_singer.Text = ".........";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(564, 301);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(171, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(42, 301);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(491, 21);
+            this.hScrollBar1.TabIndex = 9;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(336, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Mute";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(432, 217);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(564, 375);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(171, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Download";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // pic_cs
+            // 
+            this.pic_cs.Location = new System.Drawing.Point(553, 49);
+            this.pic_cs.Name = "pic_cs";
+            this.pic_cs.Size = new System.Drawing.Size(171, 223);
+            this.pic_cs.TabIndex = 2;
+            this.pic_cs.TabStop = false;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(909, 152);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(10, 10);
+            this.axWindowsMediaPlayer1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 450);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lb_singer);
+            this.Controls.Add(this.lb_song);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic_cs);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_search);
+            this.Name = "Form1";
+            this.Text = "Nghe Nhạc Online";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_song;
+        private System.Windows.Forms.Label lb_singer;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pic_cs;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_search_Click(object sender, EventArgs e)
+        {
+            string baihat = txt_search.Text;
+            WebRequest req = HttpWebRequest.Create("http://hack.2k-confessions.com/nhac.php?baihat=" + baihat);
+            req.Method = "GET";
+            string source;
+            StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream());
+            source = reader.ReadToEnd();
+           
+            dynamic stuff = JsonConvert.DeserializeObject(source);
+            string tenbh = stuff.ten;
+            string tencs = stuff.casi;
+            string thumb = stuff.thumbnail;
+            string link = stuff.url;
+            string noti = stuff.text;
+            lb_song.Text = tenbh;
+            lb_singer.Text = tencs;
+            pic_cs.ImageLocation = thumb;
+            axWindowsMediaPlayer1.URL = link;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            trackBar1.Value = 5;
+            MessageBox.Show(noti, "notification");
+ 
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //axWindowsMediaPlayer1.settings.mute = true;
+           if(axWindowsMediaPlayer1.settings.mute == true)
+            {
+                axWindowsMediaPlayer1.settings.mute = false;
+               
+            }
+            else
+            {
+                axWindowsMediaPlayer1.settings.mute = true;
+                button4.BackColor = Color.White;
+            }
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            DialogResult noti;
+            noti = (MessageBox.Show("Are You Sure ? ", "Notification", MessageBoxButtons.YesNo,MessageBoxIcon.Warning));
+            if (noti == DialogResult.Yes)
+                Application.Exit();
+
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "music *.mp3 | *.mp3";
+            dialog.FileName = lb_song.Text;
+            DialogResult result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(axWindowsMediaPlayer1.URL, dialog.FileName);
+            }
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 100;
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+
+        }
+
+        private void HScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = hScrollBar1.Value;
+        }
+    }
+}
